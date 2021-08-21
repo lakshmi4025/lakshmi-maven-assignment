9c4646d01840c973e13bbbda5f1503750f0b0f18
lakshmi <laksmi.pokuru1994@gmail.com>
2021-08-20 21:07:30 -0700

    checkin for maven project

---
+++ src/main/java/com/lakshmi/assignment/lakshmi_maven_assignment/App.java
@@ -0,0 +1,13 @@
+package com.lakshmi.assignment.lakshmi_maven_assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to maven assignment lakshmi!" );
+    }
+}
